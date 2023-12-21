97c506580ad850bd8d319bbb99d53ac1b63f0ae1
Vlad <z0l0t0m@mail.ru>
2023-12-21 16:25:03 +0300

    Add: loading

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,31 +7,28 @@ import CartProvider from "./hooks/useCart";
 import { AuthProvider } from "./hooks/useAuth";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { LoadingProvider } from "./hooks/useLoading";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <LoadingProvider>
-        <AuthProvider>
-          <CartProvider>
-            <App />
-            <ToastContainer
-              position='bottom-right'
-              autoClose={5000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme='light'
-            />
-          </CartProvider>
-        </AuthProvider>
-      </LoadingProvider>
+      <AuthProvider>
+        <CartProvider>
+          <App />
+          <ToastContainer
+            position='bottom-right'
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme='light'
+          />
+        </CartProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
